4997a3de9ef95805a8c7f7abd78d57f2f883d691
Akshat Kumar <akshat8066@gmail.com>
2025-09-06 10:52:33 +0530

    Removed unnecessary files

--- Projects/Interview/[MONOLITHIC]_Interview_Simulation.py
+++ Projects/Interview/[MONOLITHIC]_Interview_Simulation.py
@@ -9,7 +9,6 @@ import asyncio
 model_client = OpenAIChatCompletionClient(
    model = 'gemini-2.0-flash',
    api_key= 'AIzaSyBr019iRxayVl6kdeh-J4IhS5hq_YgNP8c',
-
 )
 
 interiewer = AssistantAgent(
